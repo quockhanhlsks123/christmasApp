67efe81d168f455b85f5289806c36144b9b6bf42
quockhanhlsks123 <quockhanhlsls44@gmail.com>
2023-12-13 15:47:15 +0700

    first

---
+++ src/components/SignIn.js
@@ -0,0 +1,24 @@
+import React from "react";
+import '../components/SignIn.css'
+
+class SignIn extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div className="left-container">
+                    left
+                    <div className="left-image">
+
+                    </div>
+                </div>
+
+                <div className="right-container">
+                    right
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SignIn
